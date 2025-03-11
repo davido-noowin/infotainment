286e3312425e109b7c895e37e901d2a2b611d329
David Nguyen <davido.nguyen1212@gmail.com>
2025-03-10 23:54:01 -0700

    fix aspect ratio issue
    there is now a scroll bar that appears due to the increased aspect ratio

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -6,17 +6,16 @@ export default function Canvas() {
     const [video, setVideo] = useState(videoLinksArray[0])
     
     return (
-        <main>
+        <main seamless="seamless">
             <iframe 
-                src= "https://www.youtube.com/embed/LRmNqKw6Ly0?start=600&autoplay=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&playlist=LRmNqKw6Ly0"
+                src= "https://www.youtube.com/embed/3DD4pgL5bS4?start=777&autoplay=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&playlist=3DD4pgL5bS4"
                 frameborder="0"
                 className="canvas"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
                 referrerPolicy="strict-origin-when-cross-origin" 
-                allowFullScreen
+                allowfullscreen
             >
             </iframe>
         </main>
-
     )
 }
