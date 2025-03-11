c275dc3540159297e0d69598de68c745526423ca
David Nguyen <davido.nguyen1212@gmail.com>
2025-03-10 22:26:25 -0700

    create components folder with canvas component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Canvas from './components/Canvas'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <h1>Hello World</h1>
+      <Canvas />
     </>
   )
 }
