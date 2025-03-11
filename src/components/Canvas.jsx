a6143241c6daa2adc642c40e7262e8308ea6e956
David Nguyen <davido.nguyen1212@gmail.com>
2025-03-10 23:12:07 -0700

    iframe component skeleton

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react"
+import "./Canvas.css"
+import videoLinksArray from "../assets/videoLinks.json"
+
 export default function Canvas() {
+    const [video, setVideo] = useState(videoLinksArray[0])
+    
     return (
-        <p>Canvas works</p>
+        <main>
+            <iframe 
+                src= "https://www.youtube.com/embed/LRmNqKw6Ly0?start=600&autoplay=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&playlist=LRmNqKw6Ly0"
+                frameborder="0"
+                className="canvas"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerPolicy="strict-origin-when-cross-origin" 
+                allowFullScreen
+            >
+            </iframe>
+        </main>
+
     )
 }
