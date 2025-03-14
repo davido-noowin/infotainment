49ad82d47f3a1610802371d1fc47929d2fa11546
David Nguyen <davido.nguyen1212@gmail.com>
2025-03-13 17:13:22 -0700

    refactor weather component

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,37 +1,64 @@
+import { useState, useEffect } from "react"
 import { fetchWeatherApi } from 'openmeteo';
 import "./DateTimeWeather.css"
 import weatherDescriptions from "../assets/weatherDescriptions.js"
 
 const weatherURL = "https://api.open-meteo.com/v1/forecast";
-const weatherParams = {
-    "latitude": 33.787,
-    "longitude": -117.923,
-    "hourly": "temperature_2m",
-    "daily": "weather_code",
-    "temperature_unit": "fahrenheit",
-    "wind_speed_unit": "mph",
-    "precipitation_unit": "inch",
-    "timezone": "America/Los_Angeles",
-    "forecast_days": 1,
-    "models": "gfs_seamless"
-};
 
-const weatherResponse = await fetchWeatherApi(weatherURL, weatherParams);
-const daily = weatherResponse[0].daily();
-const hourly = weatherResponse[0].hourly();
-
-export default function Weather(props) {
+function isDayOrNight() {
     const date = new Date();
     const hours = date.getHours();
     const isDayTime = hours > 6 && hours < 20;
-    let dayOrNight = isDayTime ? "day" : "night";
-    const weatherCode = daily.variables(0).valuesArray()[0].toString();
-    const temperature = Math.round(hourly.variables(0).valuesArray()[hours]);
+    return isDayTime ? "day" : "night";
+}
+
+export default function Weather(props) {
+    const weatherParams = {
+        "latitude": props.latitude,
+        "longitude": props.longitude,
+        "hourly": "temperature_2m",
+        "daily": "weather_code",
+        "temperature_unit": "fahrenheit",
+        "wind_speed_unit": "mph",
+        "precipitation_unit": "inch",
+        "timezone": "America/Los_Angeles",
+        "forecast_days": 1,
+        "models": "gfs_seamless"
+    };
+
+    const [weatherResponse, setWeatherResponse] = useState(null);
+
+    useEffect(() => {
+        async function getWeatherInfo(weatherURL, weatherParams) {
+            const weatherResponses = await fetchWeatherApi(weatherURL, weatherParams)
+            .then((data) => data[0]);
+            setWeatherResponse(weatherResponses); 
+        }
+        getWeatherInfo(weatherURL, weatherParams);
+    }, [])
+
+    let dayOrNight = isDayOrNight();
+    let weatherCode = ""
+    let temperature = 0
+    if (weatherResponse !== null) {
+        const daily = weatherResponse.daily();
+        const hourly = weatherResponse.hourly();
+        weatherCode = daily.variables(0).valuesArray()[0].toString();
+        temperature = Math.round(hourly.variables(0).valuesArray()[new Date().getHours()]);
+    }
+    
 
     return (
         <div className="weather-container">
-            <img src={weatherDescriptions[weatherCode][dayOrNight]["image"]} />
-            <h3>{temperature}° F</h3>
+            { 
+            weatherResponse != null ? 
+            <div className="weather-container">
+                <img src={weatherDescriptions[weatherCode][dayOrNight]["image"]} />
+                <h3>{temperature}° F</h3>
+            </div>
+            :
+            null
+            }
         </div>
     )
 }
