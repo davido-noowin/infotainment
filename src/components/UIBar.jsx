15f88fbb739b6344be64b82d5e0b84e867e9bfb3
David Nguyen <davido.nguyen1212@gmail.com>
2025-03-14 22:09:41 -0700

    fix volume img inconsistency, add slider

--- src/components/UIBar.jsx
+++ src/components/UIBar.jsx
@@ -4,11 +4,14 @@ import VideoSelect from "./VideoSelect"
 import Canvas from "./Canvas"
 import hamburgerButton from "../assets/uiButtons/hamburgerButton.png"
 import closeUIButton from "../assets/uiButtons/closeUIButton.png"
+import volume from "../assets/uiButtons/Volume.png"
+import noVolume from "../assets/uiButtons/NoVolume.png"
 
 export default function UIBar() {
     const [uiBarIsOpen, setUIBarIsOpen] = useState(true);
     const [videoIDFromSelector, setVideoIDFromSelector] = useState("LRmNqKw6Ly0");
     const [isMuted, setIsMuted] = useState(true);
+    const [sliderValue, setSliderValue] = useState(40);
 
     const youtubePlayerScript = useRef(null);
     const muteButton = useRef(null);
@@ -19,24 +22,26 @@ export default function UIBar() {
         script.src = "https://www.youtube.com/iframe_api";
         var firstScriptTag = document.getElementsByTagName('script')[0];
         firstScriptTag.parentNode.insertBefore(script, firstScriptTag);
-        
 
         window.onYouTubeIframeAPIReady = function() {
-            console.log("ready!")
             player = new window.YT.Player('player', {
                 events: {
+                    'onReady': onPlayerReady,
                     'onStateChange': onPlayerStateChange
                 }
             });
         }
 
-        function onPlayerStateChange() {
-            console.log("my state changed")
+        function onPlayerReady() {
+            player.setVolume(40);
         }
 
-        function mute(event) {
-            console.log(player);
+        function onPlayerStateChange() {
+            console.log("my state changed");
+        }
 
+        function mute() {
+            //console.log(player);
             player.isMuted() ? player.unMute() : player.mute();
         }
 
@@ -56,15 +61,20 @@ export default function UIBar() {
     
 
     function toggleUIMenu() {
-        setUIBarIsOpen(prevIsOpen => !prevIsOpen)
+        setUIBarIsOpen(prevIsOpen => !prevIsOpen);
     }
 
     function getVideoIDFromSelector(event) {
-        setVideoIDFromSelector(event.target.value)
+        setVideoIDFromSelector(event.target.value);
+        setIsMuted(true);
     }
 
     function toggleMuteButton() {
-        setIsMuted(prevIsMuted => !prevIsMuted)
+        setIsMuted(prevIsMuted => !prevIsMuted);
+    }
+
+    function updateVolume(event) {
+        setSliderValue(event.target.value);
     }
     
     return (
@@ -78,7 +88,22 @@ export default function UIBar() {
                         </button>
                     </div>
                     <VideoSelect handleSelect={getVideoIDFromSelector} focusedVideoID={videoIDFromSelector}/>
-                    <button ref={muteButton} onClick={toggleMuteButton}>{isMuted ? "unmute" : "mute"}</button>
+                    <div className="volume-control">
+                        <button ref={muteButton} onClick={toggleMuteButton}>
+                            <h2>Ambient Noise</h2>
+                            <img src={isMuted ? noVolume : volume}/>
+                        </button>
+                        <div className="slide-container">
+                            <input 
+                                id="volume" 
+                                type="range" 
+                                min="0" 
+                                max="100" 
+                                value={sliderValue} 
+                                onChange={updateVolume}
+                            />
+                        </div>
+                    </div>
                 </div>
                 :
                 <div className="ui-bar-no-bg">
