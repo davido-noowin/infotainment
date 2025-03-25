bbdcd2f489260ac6d9bd15ca51a02c95949e3ad0
David Nguyen <davido.nguyen1212@gmail.com>
2025-03-24 21:35:14 -0700

    add media control buttons

--- src/components/MusicBar.jsx
+++ src/components/MusicBar.jsx
@@ -1,11 +1,14 @@
 import "./styles/MusicBar.css"
 import SpotifyLogin from "./SpotifyLogin"
+import SpotifyWebPlayback from "./SpotifyWebPlayback"
 import hideMusicPlayer from "../assets/uiButtons/HideMusicPlayer.png"
+import { useState } from "react"
 
 export default function MusicBar(props) {
+    const [token, setToken] = useState("");
     return (
         <div className={`music-bar-container ${props.sideBarOpen ? "ui-open" : ""}`}>
-            <SpotifyLogin />
+            {token !== "" ? <SpotifyLogin /> : <SpotifyWebPlayback />}
             <button 
                 onClick={() => props.toggleMusicPlayer(false)}
                 className="close-player-btn"
--- src/components/SpotifyWebPlayback.jsx
+++ src/components/SpotifyWebPlayback.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./styles/SpotifyWebPlayback.css"
 
 const track = {
     name: "",
@@ -14,12 +15,23 @@ export default function SpotifyWebPlayback(props) {
             <div className="music-player-sections">
                 <div className="now-playing-side">
                     <p>Now Playing:</p>
+                    <div className="title-and-author">
+
+                    </div>
                 </div>
                 <div className="media-controls">
-
+                    <button>
+                        back
+                    </button>
+                    <button>
+                        pause
+                    </button>
+                    <button>
+                        skip
+                    </button>
                 </div>
                 <div className="volume-controls">
-
+                    <p>volume</p>
                 </div>
             </div>
         </div>
