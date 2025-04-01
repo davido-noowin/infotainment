fa86f6fb4bca372cf81bb1ecced3d7bbf16b4f8e
David Nguyen <davido.nguyen1212@gmail.com>
2025-03-31 23:52:29 -0700

    add and style search bar to spotify ui

--- src/components/SpotifyUI.jsx
+++ src/components/SpotifyUI.jsx
@@ -1,15 +1,21 @@
 import "./styles/SpotifyUI.css"
 import closeUIButton from "../assets/uiButtons/closeUIButton.png";
+import search from "../assets/uiButtons/Search.png";
 
 export default function SpotifyUI(props) {
     return (
         <div className={`${props.spotifyUIIsOpen ? "" : "hide-spotify-ui"}`}>
             <div className={`${props.tokenInfo.token !== "" ? "shadow-spotify-bg" : ""}`}></div>
             <div className="spotify-ui-container">
-                <button className="close-spotify-btn" onClick={() => props.toggleSpotifyUI(false)}>
-                    <img src={closeUIButton} />
-                </button>
-                it works
+                <header className="spotify-ui-header">
+                    <button className="close-spotify-btn" onClick={() => props.toggleSpotifyUI(false)}>
+                        <img src={closeUIButton} />
+                    </button>
+                    <form className="song-search">
+                        <button><img src={search} alt="search"/></button>
+                        <input className="search-field" type="search" placeholder="I want to listen to..." />
+                    </form>
+                </header>
             </div>
         </div>
     )
