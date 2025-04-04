77d314c60307af7f2d555a4c5e70e503e5689c9b
David Nguyen <davido.nguyen1212@gmail.com>
2025-04-04 11:13:55 -0700

    media control and pagination for playlist ui

--- src/components/SpotifyUIPlaylist.jsx
+++ src/components/SpotifyUIPlaylist.jsx
@@ -1,5 +1,12 @@
 import backArrow from "../assets/uiButtons/goBack.png"
+import pause from "../assets/uiButtons/pause.png";
+import play from "../assets/uiButtons/play.png";
+import shuffle from "../assets/uiButtons/Shuffle.png";
+import disableShuffle from "../assets/uiButtons/DisableShuffle.png";
+import leftArrow from "../assets/uiButtons/ArrowLeft.png"
+import rightArrow from "../assets/uiButtons/ArrowRight.png"
 import "./styles/SpotifyUIPlaylist.css"
+import { useState } from "react"
 
 export default function SpotifyUIPlaylist(props) {
     function closePlaylist() {
@@ -18,8 +25,24 @@ export default function SpotifyUIPlaylist(props) {
                         <h1>the new wave</h1>
                         <p>David Nguyen</p>
                     </div>
-                    <div className="playlist-btn-group">
-
+                </div>
+                <div className="playlist-btn-group">
+                    <div className="song-control-btn-group">
+                        <button className="song-control-btn">
+                            <img src={play}/>
+                        </button>
+                        <button className="song-control-btn">
+                            <img src={shuffle}/>
+                        </button>
+                    </div>
+                    <div className="pagination-btn-group">
+                        <button className="pagination-btn">
+                            <img src={leftArrow} />
+                        </button>
+                        <p className="pagination-page">1</p>
+                        <button className="pagination-btn">
+                            <img src={rightArrow} />
+                        </button>
                     </div>
                 </div>
             </div>
