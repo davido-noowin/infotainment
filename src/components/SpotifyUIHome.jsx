92abdb8ede9f1cab6583e33e64d668d944a9e293
David Nguyen <davido.nguyen1212@gmail.com>
2025-04-03 19:26:13 -0700

    spotify ui home screen fetches display name

--- src/components/SpotifyUIHome.jsx
+++ src/components/SpotifyUIHome.jsx
@@ -1,9 +1,34 @@
 import "./styles/SpotifyUIHome.css"
+import { useState, useEffect } from "react"
+import handleError from "../handleError";
 
-export default function SpotifyUIHome() {
+
+export default function SpotifyUIHome(props) {
+    const [displayName, setDisplayName] = useState("");
+
+    useEffect(() => {
+        async function getUser() {
+            const response = await fetch("https://api.spotify.com/v1/me", {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${props.tokenInfo.token}`,
+                    "Content-Type": "application/json",
+                  }
+            }).catch(handleError);
+            if (response.ok) {
+                const json = await response.json();
+                console.log(json);
+                setDisplayName(json.display_name);
+            }
+        }
+
+        getUser();
+    }, [])
+
+
     return (
         <div className="spotify-home-container">
-            Home
+            <h1 className="spotify-home-welcome">Hello, {displayName.split(" ")[0]}</h1>
         </div>
     )
 }
