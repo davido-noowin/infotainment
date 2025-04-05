888071291cdd1310c94905096877a3857a06f1dd
David Nguyen <davido.nguyen1212@gmail.com>
2025-04-04 18:52:40 -0700

    load user playlists via spotify api

--- src/components/SpotifyUIMyPlaylists.jsx
+++ src/components/SpotifyUIMyPlaylists.jsx
@@ -1,11 +1,38 @@
 import "./styles/SpotifyUIMyPlaylists.css"
 import SpotifyUIPlaylist from "./SpotifyUIPlaylist"
-import playlists from "../assets/tempPlaylists"
-import { useState } from "react"
+import handleError from "../handleError";
+import { useState, useEffect } from "react"
+
+const userPlaylists = {
+    items: [{
+        id: "",
+        images: [{ url: null }],
+        name: ""
+    }]
+}
 
 export default function SpotifyUIMyPlaylists(props) {
     const [playlistScreen, setPlaylistScreen] = useState(false);
     const [playlistID, setPlaylistID] = useState("");
+    const [playlists, setPlaylists] = useState(userPlaylists);
+
+    useEffect(()=>{
+        async function getUserPlaylists() {
+            const response = await fetch(`https://api.spotify.com/v1/me/playlists?limit=50`, {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${props.tokenInfo.token}`,
+                    "Content-Type": "application/json",
+                }
+            }).catch(handleError);
+            if (response.ok) {
+                const json = await response.json();
+                // console.log(json);
+                setPlaylists(json);
+            }
+        }
+        getUserPlaylists();
+    }, [])
 
     function selectPlaylist(id) {
         setPlaylistScreen((prev) => !prev);
