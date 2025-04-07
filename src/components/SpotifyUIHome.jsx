0532e8d5d97f31b8fc19cee5159d627969d5d955
David Nguyen <davido.nguyen1212@gmail.com>
2025-04-06 21:56:15 -0700

    integrate spotify playlist for home screen

--- src/components/SpotifyUIHome.jsx
+++ src/components/SpotifyUIHome.jsx
@@ -1,9 +1,19 @@
+import SpotifyUIPlaylist from "./SpotifyUIPlaylist";
 import "./styles/SpotifyUIHome.css"
+import { useState } from "react"
 
 export default function SpotifyUIHome(props) {
+    const [playlistScreen, setPlaylistScreen] = useState(false);
+    const [playlistID, setPlaylistID] = useState("")
+
+    function selectPlaylist(id) {
+        setPlaylistScreen((prev) => !prev);
+        setPlaylistID(id);
+    }
+
     const availablePlaylists = props.playlistsToDisplay.map((playlist) => {
         return (
-            <button key={playlist.id} className="home-playlist-btn">
+            <button key={playlist.id} className="home-playlist-btn" onClick={() => {selectPlaylist(playlist.id)}}>
                 <img className="home-playlist-img" draggable="false" src={playlist.images[0].url} alt={playlist.name} />
                 <p>{playlist.name}</p>
             </button>
@@ -12,11 +22,24 @@ export default function SpotifyUIHome(props) {
 
     return (
         <div className="spotify-home-container">
-            <div className="home-header">
+            {!playlistScreen && <div className="home-header">
                 <h1 className="spotify-home-welcome">Hello, {props.displayName.split(" ")[0]}</h1>
-            </div>
-            <div className="prev-tracks">
-                {availablePlaylists}
+            </div>}
+            <div>
+                {
+                    playlistScreen ?
+                    <SpotifyUIPlaylist 
+                        type="playlist"
+                        tokenInfo={props.tokenInfo}
+                        player={props.player}
+                        playlistID={playlistID}
+                        closePlaylist={setPlaylistScreen}
+                    />
+                    :
+                    <div className="prev-tracks">
+                        {availablePlaylists}
+                    </div>
+                }
             </div>
         </div>
     )
