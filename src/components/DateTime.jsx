5cfae54d54e1ab49e54085610b1529e5640d06bd
David Nguyen <davido.nguyen1212@gmail.com>
2025-04-14 23:39:39 -0700

    reformat datetime component code

--- src/components/DateTime.jsx
+++ src/components/DateTime.jsx
@@ -1,45 +1,53 @@
-import { useState, useEffect } from "react"
-import "./styles/DateTimeWeather.css"
+import { useState, useEffect } from "react";
+import "./styles/DateTimeWeather.css";
 
 const days = [
-    "SUNDAY",
-    "MONDAY",
-    "TUESDAY",
-    "WEDNESDAY",
-    "THURSDAY",
-    "FRIDAY",
-    "SATURDAY"
-]
+  "SUNDAY",
+  "MONDAY",
+  "TUESDAY",
+  "WEDNESDAY",
+  "THURSDAY",
+  "FRIDAY",
+  "SATURDAY",
+];
 
 const months = [
-    "JAN",
-    "FEB",
-    "MAR",
-    "APR",
-    "MAY",
-    "JUN",
-    "JUL",
-    "AUG",
-    "SEP",
-    "OCT",
-    "NOV",
-    "DEC"
-]
+  "JAN",
+  "FEB",
+  "MAR",
+  "APR",
+  "MAY",
+  "JUN",
+  "JUL",
+  "AUG",
+  "SEP",
+  "OCT",
+  "NOV",
+  "DEC",
+];
 
 export default function DateTime() {
-    const [date, setDate ] = useState(new Date());
-    useEffect(() => {
-        var timer = setInterval(() => setDate(new Date()), 1000);
+  const [date, setDate] = useState(new Date());
+  useEffect(() => {
+    var timer = setInterval(() => setDate(new Date()), 1000);
 
-        return function cleanup() {
-            clearInterval(timer);
-        }
-    });
-    return (
-        <div className="text-container">
-            <h2>- {days[date.getDay()]} -</h2>
-            <h3>{date.getDate()} {months[date.getMonth()]}. {date.getFullYear()}</h3>
-            <h3>{date.toLocaleTimeString("en-US", {hour: 'numeric', minute: 'numeric', hour12: true})}</h3>
-        </div>
-    )
+    return function cleanup() {
+      clearInterval(timer);
+    };
+  });
+  return (
+    <div className="text-container">
+      <h2>- {days[date.getDay()]} -</h2>
+      <h3>
+        {date.getDate()} {months[date.getMonth()]}. {date.getFullYear()}
+      </h3>
+      <h3>
+        {date.toLocaleTimeString("en-US", {
+          hour: "numeric",
+          minute: "numeric",
+          hour12: true,
+        })}
+      </h3>
+    </div>
+  );
 }
