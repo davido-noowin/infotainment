d33175ebc4fc250194c67947f4349d2fa7a6bdb2
David Nguyen <davido.nguyen1212@gmail.com>
2025-04-14 23:39:30 -0700

    reformat canvas component code

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,23 +1,23 @@
-import "./styles/Canvas.css"
+import "./styles/Canvas.css";
 
 export default function Canvas(props) {
-    const videoID = props.videoID
-    const youtubeEmbedURL = "https://www.youtube.com/embed/"
-    const queryString = "?start=777&autoplay=1&loop=1&mute=1&color=white&controls=0&playsinline=1&rel=0&enablejsapi=1&playlist="
-    let finalVideoURL = youtubeEmbedURL + videoID + queryString + videoID
-    
-    return (
-        <main seamless="seamless">
-            <iframe
-                id={props.id}
-                ref={props.ref} 
-                src={finalVideoURL}
-                className="canvas"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                referrerPolicy="strict-origin-when-cross-origin" 
-                allowFullScreen
-            >
-            </iframe>
-        </main>
-    )
+  const videoID = props.videoID;
+  const youtubeEmbedURL = "https://www.youtube.com/embed/";
+  const queryString =
+    "?start=777&autoplay=1&loop=1&mute=1&color=white&controls=0&playsinline=1&rel=0&enablejsapi=1&playlist=";
+  let finalVideoURL = youtubeEmbedURL + videoID + queryString + videoID;
+
+  return (
+    <main seamless="seamless">
+      <iframe
+        id={props.id}
+        ref={props.ref}
+        src={finalVideoURL}
+        className="canvas"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </main>
+  );
 }
